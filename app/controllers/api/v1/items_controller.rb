e2d540371920fc4eeda6737c9ffc90a4882ddc4b
qiyi-li <errgou@gmail.com>
2022-08-22 11:53:30 +0800

    实现items分页

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::ItemsController < ApplicationController
-end
+    def index
+        p 'index 访问了'
+        p params
+        items = Item.page(params[:page]).per(params[:num])
+        render json:{
+            resources:items,
+            pager:{
+                page:params[:page],
+                num:params[:num],
+                count:Item.count
+            }
+        }
+    end
+    def create
+        item = Item.new amount:1
+        if item.save
+            render json:{resource:item}
+
+        else 
+            render json:{errors:item.errors}
+        end
+    end
+end 
