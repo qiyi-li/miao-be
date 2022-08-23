61556a2b13a250e638a9791ec675583811bdadd1
qiyi-li <errgou@gmail.com>
2022-08-23 22:37:18 +0800

    测试validation_controller

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,6 +1,12 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
-    p params
-    head 210
-  end
+    code = SecureRandom.random_number.to_s[2..7]
+    validation_code = ValidationCode.new email:params[:email], kind:'sign_in', code:code
+
+    if validation_code.save
+      head 200
+    else
+      render json:{errors: validation_code.errors}
+    end
+end
 end
