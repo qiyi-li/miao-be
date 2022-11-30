6be45b505556593b3969c755bc429df832df0224
qiyi-li <errgou@gmail.com>
2022-11-30 23:05:05 +0800

    将多次请求429放到requests中

--- spec/requests/validation_codes_spec.rb
+++ spec/requests/validation_codes_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "ValidationCodes", type: :request do
   describe "验证码" do
-    it "验证码可以被发送" do
-      post '/api/v1/validation_codes', params: {email:'qiyi.li@outlook.com'}
+    it "发送太频繁会返回429" do
+      post '/api/v1/validation_codes', params: {email:'errgou@gmail.com'}
       expect(response).to have_http_status(200)
+      post '/api/v1/validation_codes', params: {email:'errgou@gmail.com'}
+      expect(response).to have_http_status(429)
     end
   end
 end
