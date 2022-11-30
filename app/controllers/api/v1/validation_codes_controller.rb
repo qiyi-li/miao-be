7d74c9980b37c43b9a424896c8c37ef44fa203fa
qiyi-li <errgou@gmail.com>
2022-11-30 20:49:34 +0800

    使用rspec_api_documentation测试接口并生成文档

--- app/controllers/api/v1/validation_codes_controller.rb
+++ app/controllers/api/v1/validation_codes_controller.rb
@@ -1,12 +1,12 @@
 class Api::V1::ValidationCodesController < ApplicationController
   def create
     code = SecureRandom.random_number.to_s[2..7]
-    validation_code = ValidationCode.new email:params[:email], kind:'sign_in', code:code
+    validation_code = ValidationCode.new email: params[:email], kind: "sign_in", code: code
 
     if validation_code.save
-      head 200
+      render json: { code: 0 }, status: 200
     else
-      render json:{errors: validation_code.errors}
+      render json: { errors: validation_code.errors }, status: 422
     end
-end
+  end
 end
