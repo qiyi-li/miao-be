faf8b939168fbe80525c6ed08e212febe6363767
qiyi-li <errgou@gmail.com>
2022-12-07 23:05:26 +0800

    创建jwt给前端

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,23 @@
+require 'jwt'
+class Api::V1::SessionsController < ApplicationController
+  def create
+    if Rails.env.test?
+      return render status: :unauthorized if params[:code] != "123456"
+    else
+      canSigin = ValidationCodes.exists? email: params[:email], code: params[:code], used_at: nil
+      if !canSigin
+        render status: 401, json: { errors: "验证码错误" }
+        return
+      end
+    end
+    user =User.find_by_email params[:email] 
+    if user.nil?
+      render status: 404, json: { errors: "用户不存在" }
+    else
+      hmac_scret = 'my$ecretK3y'
+      payload = {user_id: user.id}
+      token = JWT.encode payload , hmac_scret, 'HS256'
+      render status: 200, json: { jwt:  token}
+    end
+  end
+end
---
+++ spec/requests/api/v1/sessions_spec.rb
@@ -0,0 +1,14 @@
+require "rails_helper"
+
+RSpec.describe "Sessions", type: :request do
+  describe "会话" do
+    it "登录（创建会话）" do
+      User.create email: 'errgou@gmail.com'
+      post "/api/v1/session", params: { email: "errgou@gmail.com", code: "123456" }
+      expect(response).to have_http_status(200)
+      # print response.body 
+      json = JSON.parse response.body 
+      expect(json['jwt']).to be_a(String)
+    end
+  end
+end
