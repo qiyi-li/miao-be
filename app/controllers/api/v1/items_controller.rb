29122b1c42a66a2dc783e4ebe5a5b9b1d47c9499
qiyi-li <errgou@gmail.com>
2022-12-13 11:19:04 +0800

    完成items按照时间筛选功能

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,22 +1,23 @@
 class Api::V1::ItemsController < ApplicationController
-    def index
-        items = Item.page(params[:page]).per(params[:num])
-        render json:{
-            resources:items,
-            pager:{
-                page:params[:page],
-                num:params[:num],
-                count:Item.count
-            }
-        }, status:200
-    end
-    def create
-        item = Item.new amount:params[:amount]
-        if item.save
-            render json:{resource:item}
+  def index
+    items = Item.where({ created_at: params[:created_after]..params[:created_before] }).page(params[:page]).per(params[:num])
+    p items
+    render json: {
+             resources: items,
+             pager: {
+               page: params[:page],
+               num: params[:num],
+               count: Item.count,
+             },
+           }, status: 200
+  end
 
-        else 
-            render json:{errors:item.errors}
-        end
+  def create
+    item = Item.new amount: params[:amount]
+    if item.save
+      render json: { resource: item }
+    else
+      render json: { errors: item.errors }
     end
-end 
+  end
+end
--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -1,33 +1,41 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Items", type: :request do
-  describe "index by page" do
-    it "works! (now write some real specs)" do
+  describe "获取账目" do
+    it "分页" do
       15.times do
-        Item.create amount:1001
+        Item.create amount: 1001
       end
       expect(Item.count).to eq(15)
 
-      get '/api/v1/items?page=2'
+      get "/api/v1/items?page=2"
       expect(response).to have_http_status(200)
 
       json = JSON.parse(response.body)
-      expect(json['resources'].size).to eq(5)
+      expect(json["resources"].size).to eq(5)
+    end
+    it "按时间筛选" do
+      item1 = Item.create amount: 1001, created_at: Time.new(2017, 01, 02)
+      item2 = Item.create amount: 1002, created_at: Time.new(2018, 01, 01)
+      get "/api/v1/items?create_after=2017-01-01&created_before=2017-01-03"
+      expect(response).to have_http_status(200)
+      json = JSON.parse(response.body)
+      expect(json["resources"].size).to eq(1)
+      expect(json["resources"][0]["id"]).to eq(item1.id)
 
     end
   end
   describe "create" do
     it "can create an item" do
-      expect{
-        post '/api/v1/items',
-        params:{amount:99}
-      }.to change {Item.count}.by 1
+      expect {
+        post "/api/v1/items",
+             params: { amount: 99 }
+      }.to change { Item.count }.by 1
 
       expect(response).to have_http_status 200
       json = JSON.parse response.body
-      expect(json['resource']['id']).to be_an(Numeric)
-      expect(json['resource']['amount']).to eq 99
-
+      expect(json["resource"]["id"]).to be_an(Numeric)
+      expect(json["resource"]["amount"]).to eq 99
     end
   end
 end
