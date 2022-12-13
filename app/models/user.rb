2826837b1baf252a82b4071c600f870c27203857
qiyi-li <errgou@gmail.com>
2022-12-13 16:09:45 +0800

    jwt添加过期时间

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   validates :email, presence: true
   def generate_jwt
     hmac_scret = 'my$ecretK3y'
-    payload = {user_id: self.id}
+    payload = {user_id: self.id, exp: (Time.now + 2.hours).to_i }
     token = JWT.encode payload , hmac_scret, 'HS256'
     return token
   end
--- spec/requests/api/v1/me_spec.rb
+++ spec/requests/api/v1/me_spec.rb
@@ -1,10 +1,13 @@
 require "rails_helper"
+require "active_support/testing/time_helpers"
 
 RSpec.describe "Api::V1::Mes", type: :request do
+  include ActiveSupport::Testing::TimeHelpers
   describe "获取当前用户" do
     it "登陆后成功获取userid" do
       user = User.create email: "errgou@gmail.com"
       post "/api/v1/session", params: { email: "errgou@gmail.com", code: "123456" }
+      expect(response).to have_http_status(200)
       json = JSON.parse response.body
       jwt = json["jwt"]
 
@@ -13,6 +16,24 @@ RSpec.describe "Api::V1::Mes", type: :request do
       expect(response).to have_http_status(200)
       json = JSON.parse response.body
       expect(json["resource"]["id"]).to eq user.id
+    end
+    it "jwt过期" do
+      travel_to Time.now - 3.hours
+      user1 = User.create email: "1@qq.com"
+      jwt = user1.generate_jwt
+
+      travel_back
+      get "/api/v1/me", headers: { 'Authorization': "Bearer #{jwt}" }
+      expect(response).to have_http_status(401)
+    end
+    it "jwt没过期" do
+      travel_to Time.now - 1.hours
+      user1 = User.create email: "1@qq.com"
+      jwt = user1.generate_jwt
+
+      travel_back
+      get "/api/v1/me", headers: { 'Authorization': "Bearer #{jwt}" }
+      expect(response).to have_http_status(200)
     end
   end
 end
