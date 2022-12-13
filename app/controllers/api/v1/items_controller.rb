28e7d3dcbe5ac6a316c83a4b395dffe298593ad4
qiyi-li <errgou@gmail.com>
2022-12-13 15:06:31 +0800

    添加sign_in helper

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,10 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.where({ created_at: params[:created_after]..params[:created_before] }).page(params[:page]).per(params[:num])
+    current_user_id = request.env['current_user_id']
+    return head 401 if current_user_id.nil?
+    items = Item.where({ user_id: current_user_id })
+            .where({ created_at: params[:created_after]..params[:created_before] })
+            .page(params[:page])
     p items
     render json: {
              resources: items,
--- spec/requests/items_spec.rb
+++ spec/requests/items_spec.rb
@@ -2,50 +2,94 @@ require "rails_helper"
 
 RSpec.describe "Items", type: :request do
   describe "获取账目" do
-    it "分页" do
-      15.times do
-        Item.create amount: 1001
+      it "分页" do
+        user1 = User.create email: "1@qq.com"
+        user2 = User.create email: "2@qq.com"
+        11.times { Item.create amount: 100, user_id: user1.id }
+        11.times { Item.create amount: 100, user_id: user2.id }
+
+        headers = sign_in user1
+        get "/api/v1/items", headers: headers
+
+        expect(response).to have_http_status 200
+        json = JSON.parse(response.body)
+        expect(json["resources"].size).to eq 10
+        get "/api/v1/items?page=2"
+        get "/api/v1/items?page=2", headers: headers
+        expect(response).to have_http_status 200
+        json = JSON.parse(response.body)
+        expect(json["resources"].size).to eq 1
       end
-      expect(Item.count).to eq(15)
+      it "按时间筛选" do
+        item1 = Item.create amount: 100, created_at: "2018-01-02"
+        item2 = Item.create amount: 100, created_at: "2018-01-02"
+        item3 = Item.create amount: 100, created_at: "2019-01-01"
+        get "/api/v1/items?created_after=2018-01-01&created_before=2018-01-03"
+        user1 = User.create email: "1@qq.com"
+        item1 = Item.create amount: 100, created_at: "2018-01-02", user_id: user1.id
+        item2 = Item.create amount: 100, created_at: "2018-01-02", user_id: user1.id
+        item3 = Item.create amount: 100, created_at: "2019-01-01", user_id: user1.id
 
-      get "/api/v1/items?page=2"
-      expect(response).to have_http_status(200)
+        headers = sign_in user1
+        get "/api/v1/items?created_after=2018-01-01&created_before=2018-01-03", headers: headers
+        expect(response).to have_http_status 200
+        json = JSON.parse(response.body)
+        expect(json["resources"].size).to eq 2
+        expect(json["resources"][0]["id"]).to eq item1.id
+        expect(json["resources"][1]["id"]).to eq item2.id
+      end
+      it "按时间筛选（边界条件）" do
+        item1 = Item.create amount: 100, created_at: "2018-01-01"
+        get "/api/v1/items?created_after=2018-01-01&created_before=2018-01-02"
+        user1 = User.create email: "1@qq.com"
+        item1 = Item.create amount: 100, created_at: "2018-01-01", user_id: user1.id
 
-      json = JSON.parse(response.body)
-      expect(json["resources"].size).to eq(5)
-    end
-    it "按时间筛选" do
-      item1 = Item.create amount: 1001, created_at: '2017-01-01'
-      item2 = Item.create amount: 1002, created_at: '2018-01-02'
-      get "/api/v1/items?create_after=2017-01-01&created_before=2017-01-02"
-      expect(response).to have_http_status(200)
-      json = JSON.parse(response.body)
-      expect(json["resources"].size).to eq(1)
-      expect(json["resources"][0]["id"]).to eq(item1.id)
+        headers = sign_in user1
+        get "/api/v1/items?created_after=2018-01-01&created_before=2018-01-02", headers: headers
+        expect(response).to have_http_status 200
+        json = JSON.parse(response.body)
+        expect(json["resources"].size).to eq 1
+        expect(json["resources"][0]["id"]).to eq item1.id
+      end
+      it "按时间筛选（边界条件2）" do
+        item1 = Item.create amount: 100, created_at: "2018-01-01"
+        item2 = Item.create amount: 100, created_at: "2017-01-01"
+        get "/api/v1/items?created_after=2018-01-01"
+        user1 = User.create email: "1@qq.com"
+        item1 = Item.create amount: 100, created_at: "2018-01-01", user_id: user1.id
+        item2 = Item.create amount: 100, created_at: "2017-01-01", user_id: user1.id
+        headers = sign_in user1
+        get "/api/v1/items?created_after=2018-01-01", headers: headers
+        expect(response).to have_http_status 200
+        json = JSON.parse(response.body)
+        expect(json["resources"].size).to eq 1
+        expect(json["resources"][0]["id"]).to eq item1.id
+      end
+      it "按时间筛选（边界条件3）" do
+        item1 = Item.create amount: 100, created_at: "2018-01-01"
+        item2 = Item.create amount: 100, created_at: "2019-01-01"
+        get "/api/v1/items?created_before=2018-01-02"
+        user1 = User.create email: "1@qq.com"
+        item1 = Item.create amount: 100, created_at: "2018-01-01", user_id: user1.id
+        item2 = Item.create amount: 100, created_at: "2019-01-01", user_id: user1.id
 
+        headers = sign_in user1
+        get "/api/v1/items?created_before=2018-01-02", headers: headers
+        expect(response).to have_http_status 200
+        json = JSON.parse(response.body)
+        expect(json["resources"].size).to eq 1
+        expect(json["resources"][0]["id"]).to eq item1.id
+      end
     end
-    it "不传create_before、create_after的情况" do
-      item1 = Item.create amount: 1001, created_at: '2017-01-01'
-      item2 = Item.create amount: 1002, created_at: '2018-01-02'
-      get "/api/v1/items"
-      expect(response).to have_http_status(200)
-      json = JSON.parse(response.body)
-      expect(json["resources"].size).to eq(2)
-      expect(json["resources"][0]["id"]).to eq(item1.id)
-      expect(json["resources"][1]["id"]).to eq(item2.id)
+    describe "create" do
+      it "can create an item" do
+        expect {
+          post "/api/v1/items", params: { amount: 99 }
+        }.to change { Item.count }.by 1
+        expect(response).to have_http_status 200
+        json = JSON.parse response.body
+        expect(json["resource"]["id"]).to be_an(Numeric)
+        expect(json["resource"]["amount"]).to eq 99
+      end
     end
-  end
-  describe "create" do
-    it "can create an item" do
-      expect {
-        post "/api/v1/items",
-             params: { amount: 99 }
-      }.to change { Item.count }.by 1
-
-      expect(response).to have_http_status 200
-      json = JSON.parse response.body
-      expect(json["resource"]["id"]).to be_an(Numeric)
-      expect(json["resource"]["amount"]).to eq 99
-    end
-  end
 end
