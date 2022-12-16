18b250eb9987d7677ec51eb0d47290d1cd7e2e55
qiyi-li <errgou@gmail.com>
2022-12-16 17:01:32 +0800

    items可以根据happen_at分组

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,10 +1,10 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    current_user_id = request.env['current_user_id']
+    current_user_id = request.env["current_user_id"]
     return head 401 if current_user_id.nil?
     items = Item.where({ user_id: current_user_id })
-            .where({ created_at: params[:created_after]..params[:created_before] })
-            .page(params[:page])
+      .where({ created_at: params[:created_after]..params[:created_before] })
+      .page(params[:page])
     p items
     render json: {
              resources: items,
@@ -18,12 +18,30 @@ class Api::V1::ItemsController < ApplicationController
 
   def create
     # item = Item.new amount: params[:amount], tags_id: params[:tags_id], user_id: request.env['current_user_id'], happen_at: params[:happen_at]
-    item = Item.new params.permit(:amount, :happen_at, tags_id:[])
-    item.user_id = request.env['current_user_id']
+    item = Item.new params.permit(:amount, :happen_at, tags_id: [])
+    item.user_id = request.env["current_user_id"]
     if item.save
       render json: { resource: item }
     else
       render json: { errors: item.errors }, status: 422
     end
+  end
+
+  def summary
+    hash = Hash.new
+    items = Item.where({ user_id: request.env["current_user_id"] })
+      .where({ created_at: params[:created_after]..params[:created_before] })
+      .where(kind: params[:kind])
+    items.each do |item|
+      key = item.happen_at.in_time_zone('Beijing').strftime("%Y-%m-%d")
+      hash[key] ||= 0
+      hash[key] += item.amount
+    end
+    groups = hash.map { |k, v| { happen_at: k, amount: v } }
+    groups.sort! { |a, b| a[:happen_at] <=> b[:happen_at] }
+    render json: {
+      groups: groups,
+      total: items.sum(:amount),
+    }
   end
 end
